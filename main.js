c27bfdbdf66e0f86a11acdc0718ddc5f0d30c528
notsniped <72265661+notsniped@users.noreply.github.com>
2022-02-06 11:34:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 status = '';
+canvas = '';
+video = '';
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
 }
 function start() {
     document.getElementById('status').innerHTML = "Status: Detecting Objects";
 }
 function modelLoaded() {
     console.log('Model successfully loaded!');
+    status = true;
+}
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
 }
 function draw() {
-    background('#414141');
+    image(video, 0, 0, 380, 380);
 }
